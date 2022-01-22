d6312a8ee6243b4f5b4d18a0af5a786c7bcff563
justinluu8235 <justinluu8235@gmail.com>
2022-01-22 09:27:36 -0800

    added views and urls

--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('<int:question_id>/', views.detail, name="detail"),   # /polls/5/
+    path('<int:question_id>/results', views.results, name='results'), # /polls/5/reesults/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
