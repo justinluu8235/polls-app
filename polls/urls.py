c6bf92d63f287a7d92a2c8ed870b7f6d92e2f2cb
justinluu8235 <justinluu8235@gmail.com>
2022-01-22 14:13:43 -0800

    form and generic view implemented

--- polls/urls.py
+++ polls/urls.py
@@ -4,9 +4,17 @@ from . import views
 app_name='polls'
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('<int:question_id>/', views.detail, name="detail"),   # /polls/5/
-    path('<int:question_id>/results', views.results, name='results'), # /polls/5/reesults/
+    path('', views.IndexView.as_view(), name = 'index'),
+    path('<int:pk>/', views.DetailView.as_view(), name="detail"),   # /polls/5/
+    path('<int:pk>/results', views.ResultsView.as_view(), name='results'), # /polls/5/reesults/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 
 ]
+
+
+
+    ### Before conversion to generic views
+    # path('', views.index, name = 'index'),
+    # path('<int:question_id>/', views.detail, name="detail"),   # /polls/5/
+    # path('<int:question_id>/results', views.results, name='results'), # /polls/5/reesults/
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
